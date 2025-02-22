22e0fe88f100e4c2564bdd3be6ea9fae7b9913df
Atlas <acun0009@algonquinlive.com>
2025-02-22 15:15:30 -0500

    added some functionality with messaging + determinging online/offline status

--- js/main.js
+++ js/main.js
@@ -1,27 +1,73 @@
 const API_KEY = '0e8faef333f9bae4c69e220ac0f29889';
 const API_READ_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZThmYWVmMzMzZjliYWU0YzY5ZTIyMGFjMGYyOTg4OSIsIm5iZiI6MTczOTg5NjI0MC4xMzIsInN1YiI6IjY3YjRiNWIwMjBiOGRjNTQ5YWUwYzUxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ojm6gCvFPNrY1mdyVeetc9dIi4RzI5wjOZFVZAr-Iq0'
 
-const URL = 'https://api.themoviedb.org/3/search/movie';
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+
+let isOnline = navigator.onLine;
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('in init: DOMContentLoaded');
+    setUpWorker();
     addListeners();
+    pageSpecific();
     let hash = location.hash ? location.hash : '#';
-    showPage(hash);
+    showPage(hash); //maybe move this in pageSpecific???
+    console.log(isOnline)
 }
+
+/* Registers our service worker */
+function setUpWorker() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('./sw.js');
+      console.log('sw registered');
+    }
+
+    navigator.serviceWorker.addEventListener('message', gotMessage);
+  }
 
 function addListeners() {
-    document.querySelector('searchForm')?.addEventListener('submit', handleSearch);
     window.addEventListener('popstate', popped);
+    window.addEventListener('online', updateOnlineStatus)
+    window.addEventListener('offline', updateOnlineStatus)  
 }
 
+
+/* Called whenever a popstate event occurs */
 function popped(ev) {
     console.log('popstate event occured!');
     let w = ev.target;
     let hash = w.location.hash ? w.location.hash : '#';
     showPage(hash);
+}
+
+/* updates our service worker on our online/offline status */
+function updateOnlineStatus(ev) {
+    console.log('IN: updateOnlineStatus');
+
+    if(ev.type === 'online') {
+        isOnline = true;
+        sendMessage({ type: 'NETWORK_STATUS', isOnline });
+    }
+    if(ev.type === 'offline') {
+        isOnline = false;
+        sendMessage({ type: 'NETWORK_STATUS', isOnline });
+    }
+}
+
+/* Sends message to service worker */
+function sendMessage(msg) {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.ready.then((reg) => {
+			reg.active.postMessage(msg);
+		});
+	}
+}
+
+/* Listens to messages from our service worker */
+function gotMessage(ev) {
+//TODO
 }
 
 async function showPage(hash) {
@@ -38,3 +84,82 @@ async function showPage(hash) {
     hash === '#' ? document.body.className = 'home' : document.body.className = hash.replace('#', '');
     window.scrollTo(0, 0); // and scroll to the top
 }
+
+function pageSpecific() {
+    const id = document.body.className;
+    console.log(id)
+    switch (id) {
+        case 'home':
+            // add listener to the form
+            document.querySelector('#searchForm').addEventListener('submit', handleSearch); //TODO
+            // add listnever to the movie cards
+            // document.querySelector('main').addEventListener('click', handleAddCart); //TODO main to card section
+            break;
+        case 'cart':
+            // get movies in cart
+            getMoviesInCart(); //TODO
+            break;
+        default:
+    }
+}
+
+async function handleSearch(ev) {
+    ev.preventDefault();
+    console.log('IN: handleSearch')
+    let searchInput = document.getElementById('search').value;
+
+    console.log(searchInput);
+
+    try {
+        const res = await fetch(BASE_URL + `?query=${searchInput}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${API_READ_ACCESS_TOKEN}`
+            }
+        });
+
+        if(!res.ok) {
+            throw new Error(`Oops! Something bad happened!: ${res.status}`);
+        }
+
+        const data = await res.json();
+        console.log(data);
+        parseData(data);
+        return data
+    } catch(error) {
+        console.error(`There was an error when fetching your data: ${error.status}`);
+    }
+}
+
+function parseData(data) {
+    // image, movie title, duration, release date, description
+}
+
+// function handleAddCart(ev) {
+//     ev.preventDefault();
+//     console.log('IN: handleAddCart')
+// }
+
+
+/**
+ * ADD functionality to check for offline vs online (should do this in the sw)
+ * caching stretegies??? network first and revalidate (check if online first!!)
+ * need
+ * CACHES NEEDED
+ * 1. search results
+ * 2. cart items
+ * 3. rented movies
+ * 4. movie images
+ * 5. app files
+ * 
+ * SPA SPECIFIC
+ * - need to clear page whenever we navigate to a new one
+ * 
+ * OTHER
+ * CART:
+ * - need functional cart icon with logic!!
+ * - have button to rent whole cart
+ * - need to send msg to sw that updating cart needs to be relayed on all the open tabs
+ * after renting whole cart, need to redirect
+ */
--- sw.js
+++ sw.js
@@ -8,9 +8,51 @@ self.addEventListener('activate', (ev) => {
 });
 
 self.addEventListener('fetch', (ev) => {
+    console.log(`fetch request for ${ev.request.url}`);
+
+    // look through cache first!!!
+    // look up cacheing strategies
+    ev.respondWith(
+        caches.match(ev.request).then( cacheRes => {
+            return cacheRes || fetch(ev.request)
+            .then(fetchResponse => {
+                let type = fetchResponse.headers.get('content-type');
+                
+            })
+        })
+    )
+    // we respond with the response
+    // ev.respondWith( fetch(ev.request) )
 
 })
 
 self.addEventListener('message', (ev) => {
 
+    if ('type' in ev.data) {
+        // update online/offline status
+        if (ev.data.type === 'NETWORK_STATUS') {
+            self.isOnline = ev.data.isOnline;
+            console.log(`SW updated. status: ${self.isOnline}`);
+        }
+    }
+    /** 'action' types
+     * getCartList
+     * getRentedList
+     * rentMovies
+     * watchMovie
+     */
+    if ('action' in ev.data) {
+        if (ev.data.action === 'getCartList') {
+            
+        }
+        if (ev.data.action === 'getRentedList') {
+
+        }
+        if (ev.data.action === 'rentMovies') {
+
+        }
+        if (ev.data.action === 'watchMovie') {
+
+        }
+    }
 })
