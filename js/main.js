0bbb31d797d51caa0e87f6aa47c43bee9b34d22f
Atlas <acun0009@algonquinlive.com>
2025-02-22 22:13:53 -0500

    added functionality for handling search + adding to cart

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const API_KEY = '0e8faef333f9bae4c69e220ac0f29889';
 const API_READ_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZThmYWVmMzMzZjliYWU0YzY5ZTIyMGFjMGYyOTg4OSIsIm5iZiI6MTczOTg5NjI0MC4xMzIsInN1YiI6IjY3YjRiNWIwMjBiOGRjNTQ5YWUwYzUxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ojm6gCvFPNrY1mdyVeetc9dIi4RzI5wjOZFVZAr-Iq0'
 
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+const BASE_URL = 'https://api.themoviedb.org/';
 
 let isOnline = navigator.onLine;
 
@@ -12,19 +12,24 @@ function init() {
     setUpWorker();
     addListeners();
     pageSpecific();
-    let hash = location.hash ? location.hash : '#';
-    showPage(hash); //maybe move this in pageSpecific???
-    console.log(isOnline)
+    // let hash = location.hash ? location.hash : '#';
+    // showPage(hash); //maybe move this in pageSpecific???
 }
 
 /* Registers our service worker */
-function setUpWorker() {
+async function setUpWorker() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js');
-      console.log('sw registered');
+        try {
+            const registration = await navigator.serviceWorker.register('./sw.js', {
+                scope: './'
+            });
+            if (registration.active) {
+                navigator.serviceWorker.addEventListener('message', gotMessage);
+            }
+        } catch(error) {
+            console.error(`Registration failed: ${error}`);
+        }
     }
-
-    navigator.serviceWorker.addEventListener('message', gotMessage);
   }
 
 function addListeners() {
@@ -40,6 +45,7 @@ function popped(ev) {
     let w = ev.target;
     let hash = w.location.hash ? w.location.hash : '#';
     showPage(hash);
+    pageSpecific();
 }
 
 /* updates our service worker on our online/offline status */
@@ -72,7 +78,7 @@ function gotMessage(ev) {
 
 async function showPage(hash) {
     // if hash is not one of our preset IDs,
-    if(!['#', '#cart', '#rented'].includes(hash)) {
+    if(!['#', '#cart', '#rentals'].includes(hash)) {
         // replace the current history stack with '#' (home),
         history.replaceState(null, '', '#');
         // navigate to it
@@ -87,18 +93,21 @@ async function showPage(hash) {
 
 function pageSpecific() {
     const id = document.body.className;
-    console.log(id)
     switch (id) {
         case 'home':
+             console.log('home')
             // add listener to the form
             document.querySelector('#searchForm').addEventListener('submit', handleSearch); //TODO
             // add listnever to the movie cards
-            // document.querySelector('main').addEventListener('click', handleAddCart); //TODO main to card section
+            document.querySelector('#movies-search-ul').addEventListener('click', handleAddCart); //TODO main to card section
             break;
         case 'cart':
+            console.log('cart')
             // get movies in cart
-            getMoviesInCart(); //TODO
+            // getMoviesInCart(); //TODO
             break;
+        case 'rentals':
+            console.log('rentals')
         default:
     }
 }
@@ -108,10 +117,14 @@ async function handleSearch(ev) {
     console.log('IN: handleSearch')
     let searchInput = document.getElementById('search').value;
 
-    console.log(searchInput);
+    // clear search results first, if there are any
+    let ul = document.querySelector('#movies-search-ul')
+    if(ul.children.length > 0) {
+        clearSearchResults();
+    }
 
     try {
-        const res = await fetch(BASE_URL + `?query=${searchInput}`, {
+        const res = await fetch(BASE_URL + `3/search/movie?query=${searchInput}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -124,26 +137,92 @@ async function handleSearch(ev) {
         }
 
         const data = await res.json();
-        console.log(data);
-        parseData(data);
-        return data
+        const dataArr = data.results;
+
+        const movies = []
+        dataArr.forEach((movie) => {
+            let movieUrl = new URL(
+                movie?.poster_path
+                ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
+                : './img/placeholder.jpg', window.location.origin
+            );
+
+            let movieEntry = {
+                id: movie.id,
+                src: movieUrl.href,
+                title: movie?.title || 'None',
+                release_date: movie?.release_date || 'N/A',
+                rating: movie?.vote_average || 'None',
+                description: movie?.overview || 'No description found!',
+            };
+            movies.push(movieEntry);
+        });
+        buildMovieCards(movies, 'search');
     } catch(error) {
-        console.error(`There was an error when fetching your data: ${error.status}`);
+        console.error(`There was an error when fetching your data: ${error.status} ${error.message}`);
     }
 }
 
-function parseData(data) {
-    // image, movie title, duration, release date, description
+/* handles adding a movie into our cart */
+function handleAddCart(ev) {
+    console.log('IN: handleAddCart');
+    let target = ev.target;
+    let card = target.closest('.card');
+    let src = card.querySelector('img').src
+    let title = card.querySelector('.card__title').textContent;
+    let release_date = card.querySelector('[data-ref="release-date"] span').nextSibling.textContent.trim();
+    let rating = card.querySelector('[data-ref="rating"] span').nextSibling.textContent.trim();
+    let description = card.querySelector('[data-ref="description"] span').nextSibling.textContent.trim();
+
+    let msg = {
+        action: 'getCartList',
+        movie: { src, title, release_date, rating, description }
+    };
+    sendMessage(msg);
 }
 
-// function handleAddCart(ev) {
-//     ev.preventDefault();
-//     console.log('IN: handleAddCart')
-// }
+/** Builds movie cards using <template>
+ * first arg: an array of movie objects
+ * second arg: a string indication which page we are building cards for (possible values = 'search', 'cart', 'rentals')
+ */
+function buildMovieCards(movies, page = 'search') {
+    console.log('IN: build movie cards')
+    const moviesUl = document.querySelector('#movies-search-ul');
+    const temp = document.querySelector('#movies-template');
 
+    movies.forEach((movie) => {
+        const clone = temp.content.cloneNode(true);
+        clone.querySelector('.card').setAttribute('data-ref', movie.id);
+        clone.querySelector('.card__img > img').src = movie.src;
+        clone.querySelector('.card__title').textContent = movie.title;
+        // for the next three pieces of data, we need to insert text after our nested <span>, so we will insert a textNode
+        clone.querySelector('[data-ref="release-date"] span').after(document.createTextNode(` ${movie.release_date}`));
+        clone.querySelector('[data-ref="rating"] span').after(document.createTextNode(` ${movie.rating}`));
+        clone.querySelector('[data-ref="description"] span').after(document.createTextNode(` ${movie.description}`));
+        let btn = clone.querySelector('#movieBtn');
+        if (page === 'search') {
+            btn.textContent = 'Add to Cart';
+            btn.href = '#cart'
+        }
+        if (page === 'cart') {
+            btn.textContent = 'Rent';
+            btn.href = '#rent'
+        }
+        if (page === 'rentals') {
+            btn.textContent = 'Watch Now';
+            btn.href = '#watch'
+        }
+        moviesUl.appendChild(clone);
+    });
+}
+
+/* clears any previous search results */
+function clearSearchResults() {
+    const parent = document.querySelector('#movies-search-ul');
+    parent.replaceChildren();
+}
 
 /**
- * ADD functionality to check for offline vs online (should do this in the sw)
  * caching stretegies??? network first and revalidate (check if online first!!)
  * need
  * CACHES NEEDED
--- sw.js
+++ sw.js
@@ -1,10 +1,43 @@
+const version = 1;
+const appCache = 'appFiles_' + version;
+const imgCache = 'imgFiles_' + version;
+const searchCache = 'searchFiles_' + version;
+const cartCache = 'cartFiles_' + version;
+const rentalCache = 'rentalFiles_' + version;
+
+const appFiles = [
+    './',
+    './index.html',
+    './css/main.css',
+    './js/main.js',
+    './img/android-chrome-192x192.png',
+    './img/apple-touch-icon.png',
+    './img/favicon-96x96.png',
+    './img/favicon.ico',
+    './img/favicon.svg',
+    './img/placeholder.jpg',
+    './img/site.webmanifest',
+    './img/web-app-manifest-512x512.png',
+    './manifest.json'
+]
+
 self.addEventListener('install', (ev) => {
     // we can use ev.waitUntil() here
     //cache.addAll() is the same as fetch and put
+    ev.waitUntil(
+        caches.open(appCache).then((cache) => {
+            cache.addAll(appFiles);
+            console.log('cached all appfiles!')
+        })
+    )
 });
 
 self.addEventListener('activate', (ev) => {
     // this is where we delete old versions of cache
+    caches.keys().then((cacheList) => {
+        //filter out the ones that DO match the current cache names
+        return Promise.all(cacheList.filter((cache) => ![appCache, imgCache, searchCache, cartCache, rentalCache].includes(cache)).map((cache) => caches.delete(cache)));
+      });
 });
 
 self.addEventListener('fetch', (ev) => {
@@ -12,22 +45,21 @@ self.addEventListener('fetch', (ev) => {
 
     // look through cache first!!!
     // look up cacheing strategies
-    ev.respondWith(
-        caches.match(ev.request).then( cacheRes => {
-            return cacheRes || fetch(ev.request)
-            .then(fetchResponse => {
-                let type = fetchResponse.headers.get('content-type');
+    // ev.respondWith(
+    //     caches.match(ev.request).then( cacheRes => {
+    //         return cacheRes || fetch(ev.request)
+    //         .then(fetchResponse => {
+    //             let type = fetchResponse.headers.get('content-type');
                 
-            })
-        })
-    )
+    //         })
+    //     })
+    // )
     // we respond with the response
     // ev.respondWith( fetch(ev.request) )
 
 })
 
 self.addEventListener('message', (ev) => {
-
     if ('type' in ev.data) {
         // update online/offline status
         if (ev.data.type === 'NETWORK_STATUS') {
@@ -37,22 +69,22 @@ self.addEventListener('message', (ev) => {
     }
     /** 'action' types
      * getCartList
-     * getRentedList
+     * getRentalsList
      * rentMovies
      * watchMovie
      */
     if ('action' in ev.data) {
         if (ev.data.action === 'getCartList') {
-            
+            console.log(ev.data.movie);
         }
-        if (ev.data.action === 'getRentedList') {
+        if (ev.data.action === 'getRentalsList') {
 
         }
         if (ev.data.action === 'rentMovies') {
 
         }
         if (ev.data.action === 'watchMovie') {
-
+            
         }
     }
 })
